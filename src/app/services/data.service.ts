912df04202c848a2cae2fe7a4a05fccc9f55de83
Joel McKay <jtmckay2017@gmail.com>
2020-10-29 12:28:22 -0400

    More rows

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,31 +7,23 @@ import { Column } from '../models/column';
   providedIn: 'root'
 })
 export class DataService {
-  numTicketsPerCol = 100;
+  numTicketsPerCol = 10;
   numOfColumns = 15;
+  numOfBoards = 10;
   constructor() {
-    for (let colIndex = 0; colIndex < this.numOfColumns; colIndex++) {
-      this._kanbanModal[0].columns[colIndex] = new Column('Backlog', []);
-      this._kanbanModal[1].columns[colIndex] = new Column('Backlog', []);
-    }
-    for (let colIndex = 0; colIndex < this.numOfColumns; colIndex++) {
-      for (let index = 0; index < this.numTicketsPerCol; index++) {
-      this._kanbanModal[0].columns[colIndex].tasks[index] = new Task(index.toString(), 5, 'Jimenez Maxwell');
-      }
-    }
-    for (let colIndex = 0; colIndex < this.numOfColumns; colIndex++) {
-      for (let index = 0; index < this.numTicketsPerCol; index++) {
-      this._kanbanModal[1].columns[colIndex].tasks[index] = new Task(index.toString(), 5, 'Jimenez Maxwell');
+    for (let bdIndex = 0; bdIndex < this.numOfColumns; bdIndex++) {
+      this._kanbanModal[bdIndex] = new Board('Scrum Team - Mobile App Development', []);
+      for (let colIndex = 0; colIndex < this.numOfColumns; colIndex++) {
+        this._kanbanModal[bdIndex].columns[colIndex] = new Column('Backlog', []);
+        for (let index = 0; index < this.numTicketsPerCol; index++) {
+          this._kanbanModal[bdIndex].columns[colIndex].tasks[index] = new Task(index.toString(), 5, 'Jimenez Maxwell');
+        }
       }
     }
   }
 
-  private _kanbanModal: Board[] = [
-    new Board('Scrum Team - Mobile App Development', []),
-    new Board('Scrum Team - Mobile App Development', [])
-  ];
-
-  public getData(id): Board {
-    return this._kanbanModal[id];
+  private _kanbanModal: Board[] = [];
+  public getData(): Board[] {
+    return this._kanbanModal;
   }
 }
